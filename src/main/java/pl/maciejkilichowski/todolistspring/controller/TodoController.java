57b2e34a4fbef219ef62deeef6326c569ee0d9ab
Maciej Kilichowski <maciejkilichowski16@gmail.com>
2022-02-20 15:19:15 +0100

    Creat GetMapping to return data from db. Create PostMapping to save data.

--- src/main/java/pl/maciejkilichowski/todolistspring/controller/TodoController.java
+++ src/main/java/pl/maciejkilichowski/todolistspring/controller/TodoController.java
@@ -1,10 +1,27 @@
 package pl.maciejkilichowski.todolistspring.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.maciejkilichowski.todolistspring.Repository.Todo;
+import pl.maciejkilichowski.todolistspring.model.TodoEntity;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/todo")
 public class TodoController {
+    @Autowired
+    private Todo todoRepository;
 
+    @GetMapping
+    public List<TodoEntity> findAll(){
+
+        return todoRepository.findAll()
+    }
+    @PostMapping
+    public TodoEntity save(@Valid @NotNull @RequestBody TodoEntity todoEntity){
+        return todoRepository.save(todoEntity);
+    }
 }
