6b5b82943a1e545848cc4e7c1e3f0801b0144845
Maciej Kilichowski <maciejkilichowski16@gmail.com>
2022-02-20 14:41:27 +0100

    First commit, Spring initialization

---
+++ src/test/java/pl/maciejkilichowski/todolistspring/TodolistSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maciejkilichowski.todolistspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
